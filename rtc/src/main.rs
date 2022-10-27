1e2a0c12e7d0ce72ecfcc563c81963154c5bd64f
nbalcarc <nathan.balcarcel@wsu.edu>
2022-10-26 20:26:25 -0700

    Communication now working between a server and client

--- rtc/src/main.rs
+++ rtc/src/main.rs
@@ -1,3 +1,46 @@
+mod client;
+mod server;
+mod signal;
+
+use clap::{arg, command, value_parser, ArgAction, Command};
+
+
+#[macro_use]
+extern crate lazy_static;
+
 fn main() {
-    println!("Hello, world!");
+    //let matches = command!() // requires `cargo` feature
+    //    .arg(arg!(
+    //        [name] "Optional name to operate on"
+    //    ))
+    //    .arg(arg!(
+    //        -c --config <FILE> "Sets a custom config file"
+    //    ))
+    //    .arg(arg!(
+    //        -d --debug ... "Turn debugging information on"
+    //    ))
+    //    .subcommand(
+    //        Command::new("test")
+    //            .about("does testing things")
+    //            .arg(arg!(-l --list "lists test values").action(ArgAction::SetTrue)),
+    //    )
+    //    .get_matches();
+    //}
+
+    // Process the arguments with clap
+    let args = command!()
+        .about("testing this") // about this project
+        .arg(arg!( // first arg, client
+            -c --client "Run as client instead of server"
+        ))
+        .get_matches(); // run clap, can be omitted to save layout to a variable
+
+    //println!("{:?}", args.get_one::<bool>("client"));
+
+    if *args.get_one::<bool>("client").unwrap() {
+        client::main();
+    } else {
+        server::main();
+    }
+
 }
