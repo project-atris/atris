c7bfe6992a9d6ab1cc36baec2c3c12dd3f057116
nbalcarc <nathan.balcarcel@wsu.edu>
2022-10-29 16:07:07 -0700

    Finished showing final compression numbers for all algorithms supported by flate2

--- rtc/src/compress.rs
+++ rtc/src/compress.rs
@@ -1,50 +1,65 @@
 use flate2::Compression;
 //use flate2::write::ZlibEncoder;
-use flate2::write::GzEncoder;
-use flate2::read::GzDecoder;
+use flate2::write::{GzEncoder, ZlibEncoder, DeflateEncoder};
+use flate2::read::{GzDecoder, ZlibDecoder, DeflateDecoder};
 use std::io::prelude::*;
 use std::fs;
-use std::env;
 use crate::signal;
 use std::str;
 
 pub fn main() {
-    let mut raw: Vec<u8>;
-    let mut com: Vec<u8>;
-    let mut braw: Vec<u8>;
-    let mut bcom: Vec<u8>;
-    let mut lens: [usize; 4];
-    let mut cur_len: usize;
-
     let list = ["basic", "short", "long", "random"];
 
     //for name in std::array::IntoIter::into_iter(list) {
     for name in list.into_iter() {
 
-        raw = read_file(&["./text_", name, ".txt"].join(""));
-        com = compress(raw.clone());
-        braw = signal::encode(&String::from_utf8(raw.clone()).unwrap()).as_bytes().to_vec();
-        bcom = compress(braw.clone());
+        // raw
+        let raw = read_file(&["./text_", name, ".txt"].join(""));
+        let braw = signal::encode(&String::from_utf8(raw.clone()).unwrap()).as_bytes().to_vec();
 
-        lens = [raw.len(), com.len(), braw.len(), bcom.len()];
-        cur_len = *lens.iter().min().unwrap();
+        // gzip
+        let com_g = compress_g(raw.clone());
+        let bcom_g = compress_g(braw.clone());
+
+        // zlib
+        let com_z = compress_z(raw.clone());
+        let bcom_z = compress_z(braw.clone());
+
+        // deflate
+        let com_d = compress_d(raw.clone());
+        let bcom_d = compress_d(braw.clone());
+
+        let lens = [raw.len(), braw.len(), com_g.len(), bcom_g.len(), com_z.len(), bcom_z.len(), com_d.len(), bcom_d.len()];
+        let cur_len = *lens.iter().min().unwrap();
 
         println!("-- READING: text_{}.txt --", name);
-        println!("RAW:  {:?}", raw.len());
-        println!("COM:  {:?}", com.len());
-        //println!("DEC: {:?}", decompress(com).len());
-        println!("BRAW: {:?}", braw.len());
-        println!("BCOM: {:?}", bcom.len());
+        println!("RAW:      {:?}", lens[0]);
+        println!("B64:      {:?}", lens[1]);
+        println!("Gzip_RAW: {:?}", lens[2]);
+        println!("Gzip_B64: {:?}", lens[3]);
+        println!("Zlib_RAW: {:?}", lens[4]);
+        println!("Zlib_B64: {:?}", lens[5]);
+        println!("DEFL_RAW: {:?}", lens[6]);
+        println!("DEFL_B64: {:?}", lens[7]);
         println!("-->{}\n",
             if cur_len == lens[0] {
                 "RAW"
             } else if cur_len == lens[1] {
-                "COM"
+                "B64"
             } else if cur_len == lens[2] {
-                "BRAW"
+                "Gzip_RAW"
+            } else if cur_len == lens[3] {
+                "Gzip_B64"
+            } else if cur_len == lens[4] {
+                "Zlib_RAW"
+            } else if cur_len == lens[5] {
+                "Zlib_B64"
+            } else if cur_len == lens[6] {
+                "DEFL_RAW"
             } else {
-                "BCOM"
+                "DEFL_B64"
             }
+            
         );
     }
 }
@@ -54,8 +69,20 @@ fn read_file(target: &str) -> Vec<u8> {
         .expect("Should have been able to read the file")
 }
 
-fn compress(inp: Vec<u8>) -> Vec<u8> {
+fn compress_g(inp: Vec<u8>) -> Vec<u8> {
     let mut encryptor = GzEncoder::new(Vec::new(), Compression::default());
+    encryptor.write_all(inp.as_slice());
+    encryptor.finish().unwrap()
+}
+
+fn compress_z(inp: Vec<u8>) -> Vec<u8> {
+    let mut encryptor = ZlibEncoder::new(Vec::new(), Compression::default());
+    encryptor.write_all(inp.as_slice());
+    encryptor.finish().unwrap()
+}
+
+fn compress_d(inp: Vec<u8>) -> Vec<u8> {
+    let mut encryptor = DeflateEncoder::new(Vec::new(), Compression::default());
     encryptor.write_all(inp.as_slice());
     encryptor.finish().unwrap()
 }
