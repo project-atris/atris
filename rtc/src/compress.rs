b1c8761cb616476d6b430d7e783f649cbf5d4fe9
nbalcarc <nathan.balcarcel@wsu.edu>
2022-10-29 15:39:11 -0700

    Compress test now compares base64 as well

--- rtc/src/compress.rs
+++ rtc/src/compress.rs
@@ -5,24 +5,47 @@ use flate2::read::GzDecoder;
 use std::io::prelude::*;
 use std::fs;
 use std::env;
+use crate::signal;
 use std::str;
 
 pub fn main() {
     let mut raw: Vec<u8>;
     let mut com: Vec<u8>;
+    let mut braw: Vec<u8>;
+    let mut bcom: Vec<u8>;
+    let mut lens: [usize; 4];
+    let mut cur_len: usize;
 
     let list = ["basic", "short", "long", "random"];
 
     //for name in std::array::IntoIter::into_iter(list) {
     for name in list.into_iter() {
-        println!("-- READING: text_{}.txt --", name);
+
         raw = read_file(&["./text_", name, ".txt"].join(""));
         com = compress(raw.clone());
-        println!("RAW: {:?}", raw.len());
-        println!("COM: {:?}", com.len());
-        println!("DEC: {:?}", decompress(com).len());
-        println!("");
-        
+        braw = signal::encode(&String::from_utf8(raw.clone()).unwrap()).as_bytes().to_vec();
+        bcom = compress(braw.clone());
+
+        lens = [raw.len(), com.len(), braw.len(), bcom.len()];
+        cur_len = *lens.iter().min().unwrap();
+
+        println!("-- READING: text_{}.txt --", name);
+        println!("RAW:  {:?}", raw.len());
+        println!("COM:  {:?}", com.len());
+        //println!("DEC: {:?}", decompress(com).len());
+        println!("BRAW: {:?}", braw.len());
+        println!("BCOM: {:?}", bcom.len());
+        println!("-->{}\n",
+            if cur_len == lens[0] {
+                "RAW"
+            } else if cur_len == lens[1] {
+                "COM"
+            } else if cur_len == lens[2] {
+                "BRAW"
+            } else {
+                "BCOM"
+            }
+        );
     }
 }
 
