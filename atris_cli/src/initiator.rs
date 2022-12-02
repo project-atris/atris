a55f7c5684c98720d6263d39edb01c1eda77409f
Marcel Mukundi <45983298+MMukundi@users.noreply.github.com>
2022-12-02 12:03:32 -0800

    Fixed initiator/responder bug

--- atris_cli/src/initiator.rs
+++ atris_cli/src/initiator.rs
@@ -1,9 +1,10 @@
-use std::io::stdin;
+use std::io::{stdin, stdout};
 
 use atris_client_lib::atris_common::authenticate_user::AuthenticateUserResponse;
 use atris_client_lib::comms::responder::AtrisResponder;
 use atris_client_lib::comms::{initiator::AtrisInitiator, AtrisConnection};
 use atris_client_lib::{http_auth::AtrisAuth, AtrisAuthClient};
+use std::io::Write;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -17,27 +18,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let client = AtrisAuth::new()?;
         client.create_user(user, pass).await?;
         let initiator = AtrisInitiator::new(AtrisConnection::new().await?).await?;
-        let initiator_string = initiator.encoded_local_description()?;
+        let fake_initiator_string = initiator.encoded_local_description()?;
         let auth = client
-            .authenticate_user(user, pass, &initiator_string)
+            .authenticate_user(user, pass, &fake_initiator_string)
             .await??;
         Ok((initiator, client, auth))
     }
     // Create the client to the authorization server
-    let (_init, client, session) = for_user("terrior", "password").await?;
-    let responder = AtrisResponder::new().await?;
-    print!("Who do you want to talk to? ");
+    let (init, client, session) = for_user("marcel", "test").await?;
+    init.close().await?;
+    
+    let mut out = stdout();
+    write!(out,"Who do you want to talk to? ");
+    out.flush();
+
     let other_username = stdin().lines().next().ok_or("No terminal input!")??;
-    let room = dbg!(
-        client
-            .create_room(session.session_id.clone(), &other_username)
-            .await
-    )??;
+    let room = client
+    .create_room(session.session_id.clone(), &other_username)
+    .await??;
     println!("Ask them to join you!\nRoom ID: {}", room.room_id);
+    let responder = AtrisResponder::new().await?;
     let (responder_string, channel_future) = responder
         .open_channel_with::<String>(&room.initiator_string)
         .await?;
-    let _ = dbg!(
+    // dbg!(&room.initiator_string);
+    let room_symm_key =
         client
             .set_room_responder(
                 room.room_id,
@@ -46,8 +51,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 &responder_string
             )
             .await
-    )??;
+    ??;
+
+    let mut out = stdout();
+    writeln!(out,"Room symmetric key received");
+    out.flush();
+
     let channel = channel_future.await.ok_or("No channel recieved!")?;
+
+    let mut out = stdout();
+    writeln!(out,"Connection established!");
+    out.flush();
 
     channel.io_loop().await?;
     Ok(())
